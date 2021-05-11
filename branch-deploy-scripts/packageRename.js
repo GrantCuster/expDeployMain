2acc6eeffa6cb84d1071ce59a7f5b5cb6e5678bf
Grant Custer <grantcuster@gmail.com>
2021-05-11 10:25:14 -0400

    correct branch name

--- branch-deploy-scripts/packageRename.js
+++ branch-deploy-scripts/packageRename.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("package.json"));
 
-data.homepage = "https://grantcuster.github.io/branches/" + process.argv[2];
+data.homepage =
+  "https://grantcuster.github.io/expDeployBranches/" + process.argv[2];
 
 fs.writeFileSync("package.json", JSON.stringify(data, null, 4));
